b8534e6b396ea92c345c6a60f61eefbf4e2a632d
cuong9565 <cuongmanhle0905@gmail.com>
2025-10-14 21:27:59 +0700

    Cuong push location

--- frontend/src/app/dashboard/[dashboardRole]/bus-gps/page.tsx
+++ frontend/src/app/dashboard/[dashboardRole]/bus-gps/page.tsx
@@ -1,3 +1,5 @@
+import {Navigation, Clock} from 'lucide-react'
+
 export default function BusGp(){
   return (
       <div className="w-[100%] h-[100%] bg-[#f9fafb]">
@@ -133,18 +135,31 @@ export default function BusGp(){
 
           <div className="w-[95%] mb-[20px] p-[20px_30px] bg-[#ffffff] rounded-xl border border-solid border-[#e5e5e5]">
             <div className="text-[1.5em]">
-              <p>Live Bus Tracking</p>
-              <p>Live Bus Tracking</p>
-              <p>Live Bus Tracking</p>
-              <p>Live Bus Tracking</p>
-              <p>Live Bus Tracking</p>
-              <p>Live Bus Tracking</p>
-              <p>Live Bus Tracking</p>
-              <p>Live Bus Tracking</p>
+              Live Bus Tracking
             </div>
 
-            <div className="flex justify-between items-start mt-[50px] gap-[10px] p-[20px] rounded-xl border border-[#9e9d9d]">
-              
+            <div className="mt-[50px] rounded-xl border border-dashed border-[#9e9d9d] bg-[#eff7fe]">
+              <div className="flex flex-col justify-center items-center gap-[20px] w-full h-full p-[40px_20px]">
+                <div className="">
+                  <Navigation size='50px' className='text-[#1b61fc]'/>
+                </div>
+                <div className="text-base text-muted-foreground">
+                  Real-Time GPS Tracking
+                </div>
+                <div className="text-sm text-muted-foreground max-w-[40%] text-center">
+                  Track your child's bus location in real-time. The map shows current position, route path, and estimated arrival time.
+                </div>
+                <div className="p-[15px_15px] bg-[#ffffff] flex flex-col gap-[5px] rounded-lg shadow-[rgba(0, 0, 0, 0.24)] shadow-md">
+                  <div className="flex items-center gap-[12px]">
+                    <div className="w-[13px] h-[13px] bg-[#1b61fc] rounded-[100%]"><br /></div>
+                    <div className="text-sm">Current Location: 456 Main Street</div>
+                  </div>
+                  <div className="flex items-center gap-[10px]">
+                    <Clock color='#1b61fc' className='w-[15px] h-[15px]'/>
+                    <div className="text-sm text-muted-foreground">Estimated arrival: 5 minutes</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
 
