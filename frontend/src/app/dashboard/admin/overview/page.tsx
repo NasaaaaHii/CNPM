4bb6843dc161a6d801fffc312becdd38c34499e2
NHanh <nha261105@gmail.com>
2025-11-03 08:44:33 +0700

    update

--- frontend/src/app/dashboard/admin/overview/page.tsx
+++ frontend/src/app/dashboard/admin/overview/page.tsx
@@ -70,7 +70,7 @@ export default function Overview() {
         <div>
           <Card className="rounded-xl border border-gray-200">
             <CardHeader>
-              <CardTitle>Tổng quan các tuyến đường trong ngày</CardTitle>
+              <CardTitle>Tổng quan các tuyến đường trong ngày trên map</CardTitle>
             </CardHeader>
             <CardContent className="p-2">
               <div className="w-full h-full rounded-sm">
