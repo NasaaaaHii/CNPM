e67ac17ba68c2ad0e9abb253bcc435ce53bce23e
NHanh <nha261105@gmail.com>
2025-11-22 22:11:07 +0700

    update all realtime tracking

--- backend/src/modules/bus/bus.service.ts
+++ backend/src/modules/bus/bus.service.ts
@@ -1,9 +1,8 @@
 import supabase from "../../config/supabaseClient.js";
 
 export const getAllBus = async () => {
-    const { data: buses, error: busesError } = await supabase
-        .from('schedule')
-        .select(`
+  const { data: buses, error: busesError } = await supabase.from("schedule")
+    .select(`
             *, 
             bus (
                 bus_id,
@@ -17,35 +16,46 @@ export const getAllBus = async () => {
                 phone_number
             )
         `);
-    if (busesError) {
-        throw new Error(`Error fetching bus: ${busesError.message}`);
-    }
+  if (busesError) {
+    throw new Error(`Error fetching bus: ${busesError.message}`);
+  }
 
-    return buses;
-}
+  return buses;
+};
 
-export const updateBus = async (bus_id : number, updateData: Partial<{ license_plate_number : string;
-                number_of_seats: number;
-                bus_status: string }>) => {
-    const {data, error} = await supabase
-        .from('bus')
-        .update(updateData)
-        .eq('bus_id', bus_id);
-    if (error) {
-        throw new Error(`Error updating bus: ${error.message}`);
-    }
-    return data;
-}   
+export const updateBus = async (
+  bus_id: number,
+  updateData: Partial<{
+    license_plate_number: string;
+    number_of_seats: number;
+    bus_status: string;
+  }>
+) => {
+  const { data, error } = await supabase
+    .from("bus")
+    .update(updateData)
+    .eq("bus_id", bus_id);
+  if (error) {
+    throw new Error(`Error updating bus: ${error.message}`);
+  }
+  return data;
+};
 
-export const addBus = async (busData: Partial<{license_plate_number : string, number_of_seat: number}>) => {
-    const {data, error} = await supabase
-        .from('bus')
-        .insert([{
-            license_plate_number: busData.license_plate_number,
-            number_of_seats: busData.number_of_seat
-        }]).select().single();
-    if (error) {
-        throw new Error(`Error adding bus: ${error.message}`);
-    }
-    return data;
-}
+export const addBus = async (
+  busData: Partial<{ license_plate_number: string; number_of_seat: number }>
+) => {
+  const { data, error } = await supabase
+    .from("bus")
+    .insert([
+      {
+        license_plate_number: busData.license_plate_number,
+        number_of_seats: busData.number_of_seat,
+      },
+    ])
+    .select()
+    .single();
+  if (error) {
+    throw new Error(`Error adding bus: ${error.message}`);
+  }
+  return data;
+};
